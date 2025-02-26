567c7564126fee5a82ebb9d3ebad48e8557dccba
Joshua_Harty <89135946+slimboi34@users.noreply.github.com>
2025-02-26 14:18:00 +0000

    Fix env issues & add wait-for-db in Dockerfile

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -13,18 +13,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+import os
+import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-    )
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
 }
 
 
